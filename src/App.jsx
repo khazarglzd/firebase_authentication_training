e6f9dc7579a12db58d38d4ff57f93861bf252c65
khazarglzd <khazarguluzade@gmail.com>
2024-10-18 00:01:03 +0400

    creating signUp functionality with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import RouterConfig from "./config/RouterConfig"
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
 
 const App = () => {
   return (
     <div className='container' >
       <Navbar />
       <RouterConfig />
+      <ToastContainer position='top-right' autoClose={2000} />
     </div>
   )
 }
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import "../css/Auth.css"
+import { toast } from 'react-toastify';
+import { auth } from '../Firebase';
 
 
 const Auth = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const signUp = async () => {
+        try {
+            const response = await createUserWithEmailAndPassword(auth, email, password)
+            const user = response.user
+            if (user) {
+                toast.success("The user created")
+                setEmail('')
+                setPassword("")
+            }
+        }
+        catch (error) {
+            toast.error(error.message)
+        };
+
+    }
+
     return (
         <div className='auth'>
             <h3 className='auth-header'>Sign In / Sign Up</h3>
             <div className='auth-inputs'>
-                <input type="text" placeholder='Add email address...' />
-                <input type="password" placeholder='Add password...' />
+                <input type="text" value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder='Add email address...' />
+                <input type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} placeholder='Add password...' />
             </div>
             <div className='auth-btn-div' >
                 <button className='sign-in-btn'>Sign In</button>
-                <button className='sign-up-btn'>Sign Up</button>
+                <button className='sign-up-btn' onClick={signUp} >Sign Up</button>
                 <button className='sign-in-google-btn'>Sign in with Google </button>
             </div>
         </div>
