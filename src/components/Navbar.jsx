85ee1792a22ed3eaf33a6373242e5b865ea7a6bd
khazarglzd <khazarguluzade@gmail.com>
2024-10-23 01:05:58 +0400

    creating logOut functionality with firebase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import "../css/Navbar.css"
+import { auth } from "../Firebase"
+import { signOut } from 'firebase/auth'
+import { toast } from 'react-toastify'
 
 const Navbar = () => {
 
     const navigate = useNavigate()
 
+    const logOut = () => {
+        toast.success("Logging Out");
+        setTimeout(() => {
+            signOut(auth)
+            navigate("/auth")
+        }, 2000);
+    }
+
     return (
         <div className='navbar' >
             <div className='navbar-left' onClick={() => navigate("/auth")}>Firebase</div>
-            <div className='navbar-right' >Log Out</div>
+            <div className='navbar-right' onClick={logOut} >Log Out</div>
         </div>
     )
 }
