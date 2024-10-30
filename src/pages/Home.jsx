5ee9c4d615cbe5c04c08eb3b897cdc62b98e9b64
khazarglzd <khazarguluzade@gmail.com>
2024-10-30 20:33:10 +0400

    last commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '../Firebase';
 
 const Home = () => {
+
+    const [user, setUser] = useState("")
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (userCredential) => {
+            if (userCredential) {
+                setUser(userCredential.email)
+            }
+        })
+    }, [])
+
     return (
-        <div>Home</div>
+        <h2>Welcome {user}</h2>
     )
 }
 
